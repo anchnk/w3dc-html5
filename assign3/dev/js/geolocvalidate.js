d425ba8e2599cc33a95e69935aff7bb4512d8f8d
Klaus <anachronik@gmail.com>
2013-09-08 17:55:19 +0200

    Removed useless js code, enhanced comments and beautified code, fix a big with password validation

--- assign3/dev/js/geolocvalidate.js
+++ assign3/dev/js/geolocvalidate.js
@@ -1,181 +1,200 @@
+/*jslint browser: true */
+/*global google, console*/
+// Do not spoil JS Global Scope/
+// AMD approach would be better
 (function() {
 
-    // GELOCALISATION
+    "use strict";
+
+    // START GEOLOCALISATION PART---------------------------------------------
+    // Script Global Variables
     var geo_button = document.createElement('button'),
-        coords, address,
+        address,
         input_elms,
-        inv_elm,
-        hints;
-    
-    if(navigator.geolocation) {
+        hints,
+        addBtnFunction,
+        getPosFunction,
+        geoDecPosFunction,
+        errHandlFunction,
+        updateFormFunction;
 
-        // TODO debug this one
-        // function insertGoogleMapAPI() {
+    function geolocation() {
+        if (navigator.geolocation) {
 
-        //     // Google Maps API
-        //     var gmap_script_src = "https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false";
-        //     var gmap_script = document.createElement('script');
-        //     // Set script src to google map api src
-        //     gmap_script.src= gmap_script_src;
+            /*  Insert geolocalisation button using JS
+             *  If JS is disabled and geolocalisation not enable 
+             *  The button won't work so it's useless to display it.
+             *  So only add the button if JS is enable and geolocation is supported
+             */
+            addBtnFunction = function addButton() {
 
-        //     // Insert before the current script and returns so that we can check if it loads 
-        //     document.body.insertBefore(gmap_script, document.getElementsByTagName('script')[0]);
-        //     return gmap_script;
-        // }
+                var address_ul_elem = document.querySelector('#address'),
+                    address_ul_first_item = document.querySelector('#address>li:first-child');
 
-        // Add the button using JS, useless if JS is disable to see that
-        // button that won't have any effect
-        function addButton() {
+                // Init geolocation button attributes
+                geo_button.classList.add('geobutton');
+                geo_button.innerHTML = 'locate me';
 
-            var address_ul_elem = document.querySelector('#address');
-            var address_ul_first_item = document.querySelector('#address>li:first-child');
-    
-            // init button attributes
-            geo_button.classList.add('geobutton');
-            geo_button.innerHTML = 'locate me';
+                // add our button to the DOM tree
+                address_ul_elem.insertBefore(geo_button, address_ul_first_item);
+            };
 
-            // address_ul_elem.appendChild(geo_button);
-            address_ul_elem.insertBefore(geo_button, address_ul_first_item);
-        }
+            // Update form's address fieldset with geodecoded location
+            updateFormFunction = function updateFormWithPosition() {
 
-        // This function is triger when someone click on the locate button
-        function getPosition() {
-            navigator.geolocation.getCurrentPosition(geoDecodePosition);
-        }
+                // Get form adress fields elements
+                var street_adr1_elm = document.getElementById('street_address1'),
+                    street_adr2_elm = document.getElementById('street_address2'),
+                    zipcode_elm = document.getElementById('zipcode'),
+                    city_elm = document.getElementById('city'),
+                    country_elm = document.getElementById('country'),
+                    address_comps = address.address_components,
+                    res,
+                    i;
 
-        // Ok we got access to geolocation
-        function geoDecodePosition(position) {
+                // Fill form address fields according to objects matching properties
+                for (i in address_comps) {
+                    if (address_comps.hasOwnProperty(i)) {
+                        res = address_comps[i].long_name;
+                        switch (address_comps[i].types[0]) {
 
-            var my_lat = position.coords.latitude,
-                my_long = position.coords.longitude,
-                geocoder = new google.maps.Geocoder();
-                latlng = new google.maps.LatLng(my_lat, my_long);
-                
-            geocoder.geocode({'latLng': latlng}, function(results, status) {
-                if (status === google.maps.GeocoderStatus.OK) {
-                    address = results[0];
-                    updateFormWithPosition();
+                        case 'street_number':
+                            street_adr1_elm.value = res;
+                            break;
+
+                        case 'route':
+                            street_adr1_elm.value += ' ' + res;
+                            break;
+
+                        case 'locality':
+                            city_elm.value = res;
+                            break;
+
+                        case 'sublocality':
+                            street_adr2_elm.value = res;
+                            break;
+
+                        case 'postal_code':
+                            zipcode_elm.value = res;
+                            break;
+
+                        case 'country':
+                            country_elm.value = res;
+                            break;
+                        }
+                    }
                 }
+            };
+
+            // OK: we got a position to decode using gmaps' geodecoder
+            geoDecPosFunction = function geoDecodePosition(position) {
+
+                var latlng,
+                    my_lat = position.coords.latitude,
+                    my_long = position.coords.longitude,
+                    geocoder = new google.maps.Geocoder();
+
+                latlng = new google.maps.LatLng(my_lat, my_long);
+
+                geocoder.geocode({'latLng': latlng}, function(results, status) {
+                    if (status === google.maps.GeocoderStatus.OK) {
+                        // According to Gmaps API first result is the more accurate
+                        address = results[0];
+                        updateFormFunction();
+                    }
+                });
+            };
+
+            // NOT OK: Something went wrong when trying to get user location
+            errHandlFunction = function errorHandler() {
+                console.log('oups error');
+                // see example + some work to let user notice about the error
+            };
+
+            // Fired when geolocation button is clicked click event
+            getPosFunction = function getPosition() {
+                navigator.geolocation.getCurrentPosition(geoDecPosFunction, errHandlFunction);
+            };
+
+            addBtnFunction();
+            geo_button.addEventListener('click', function(e) {
+                e.preventDefault(); // prevent submit form
+                getPosFunction();
             });
         }
+    }
+    // END GEOLOCALISATION PART---------------------------------------------
 
-        // Oups something went wrong when trying to geolocate user
-        function errorHandler() {
-            console.log('oups error');
-            // see example + some work to let user notice about the error
-        }
+    // START VALIDATION PART------------------------------------------------
+    function formvalidation() {
+        var i;
 
-        function updateFormWithPosition() {
-            // Get form adress fields elements
-            var street_adr1_elm = document.getElementById('street_address1'),
-                street_adr2_elm = document.getElementById('street_address2'),
-                zipcode_elm = document.getElementById('zipcode'),
-                city_elm = document.getElementById('city'),
-                country_elm = document.getElementById('country'),
-                address_comps = address.address_components,
-                res;
+        input_elms = document.getElementsByTagName('input');
 
-            // Fill form address fields according to objects matching properties
-            for (i in address_comps) {
+        // function check inputs validity as well as passwords matches
+        function checkValidity(elm) {
 
-                res = address_comps[i].long_name;
+            var inv_elm,
+                passwd_elm;
 
-                switch(address_comps[i].types[0]) {
-                    case 'street_number':
-                        street_adr1_elm.value = res;
-                        break;
+            // get invalid pseudo selector input elements
+            inv_elm = document.querySelector(':invalid');
 
-                    case 'route': 
-                         street_adr1_elm.value += ' ' + res;
-                        break;
 
-                    case 'locality':
-                        city_elm.value = res;
-                        break;
-
-                    case 'sublocality':
-                        street_adr2_elm.value = res;
-
-                    case 'postal_code':
-                        zipcode_elm.value = res;
-                        break;
-
-                    case 'country':
-                        country_elm.value = res;
-                        break;
+            // only check invalidity if the user has tipped something
+            if (elm.value.length !== 0) {
+                // if current element is invalid add visual style to it
+                if (inv_elm === elm) {
+                    elm.style.backgroundColor = '#e19d9d !important';
+                    elm.classList.add('invalid');
                 }
-            }
-        }
 
-        // this script is using Google API so useless to make it works
-        // if we can't reach googl map API
-        // if(insertGoogleMapAPI()) {
-            addButton();
-            geo_button.addEventListener('click', function(e) {
-                e.preventDefault();
-                getPosition();
-            })
-        // }
-    }
-
-    // VALIDATION
-    input_elms = document.getElementsByTagName('input');
-
-    // add event listener to form input element
-    // so that validity is checked when user leave the input field
-    for (var i=0; i<input_elms.length; i = i+1) {
-        input_elms[i].addEventListener('blur', function() {
-            checkValidity(this);
-        });
-    }
-
-    // function check inputs validity as well as passwords matches
-    function checkValidity(elm) {
-
-        var submit_btn,
-            inv_elm, 
-            passwd_elm,
-            validityState_obj;
-
-        // get invalid pseudo selector input elements
-        inv_elm = document.querySelector(':invalid');
-
-        // only check invalidity if the user has tipped something
-        if (elm.value.length !== 0) {
-            // if current element is invalid add visual style to it
-            if(inv_elm === elm) {
-                elm.classList.add('invalid');        
-            }
-
-            // special case of password matching
-            if (elm.id === 'confirm_password') {
-                // get previous password element
-                passwd_elm = document.getElementById('enter_password');
-                // compare passwords
-                if(elm.value.length !== passwd_elm.value.length) {
-                    elm.setCustomValidity('length does not match');
-                } else {
-                    if (elm.value !== passwd_elm.value) {
-                        elm.setCustomValidity('password do not match');
-                        elm.classList.remove('valid');
+                // special case of password matching
+                if (elm.id === 'confirm_password') {
+                    // get previous password element
+                    passwd_elm = document.getElementById('enter_password');
+                    // compare passwords
+                    if (elm.value.length !== passwd_elm.value.length) {
+                        elm.setCustomValidity('length does not match');
                         elm.classList.add('invalid');
                     } else {
-                        elm.classList.remove('invalid');
-                        elm.classList.add('valid');
+                        if (elm.value !== passwd_elm.value) {
+                            elm.setCustomValidity('password do not match');
+                            elm.classList.remove('valid');
+                            elm.classList.add('invalid');
+                            console.log(elm.classList);
+                        } else {
+                            elm.setCustomValidity('');
+                            elm.classList.remove('invalid');
+                            elm.classList.add('valid');
+                            console.log(elm.classList);
+                        }
                     }
                 }
             }
         }
-    }
 
-    // some more work to do there
-    function showHint() {
-
-        hints = document.getElementsByClassName('hint');
-        for (var i=0; i<hints.length; i++) {
-            hints[i].style.display = 'visible';
+        // add event listener to form input element
+        // so that validity is checked when user leave the input field
+        for (i = 0; i < input_elms.length; i = i + 1) {
+            
+            input_elms[i].addEventListener('blur', function() {
+                checkValidity(this);
+            });
+            
         }
+
+        // some more work to do there
+        // function showHint() {
+        //     var i;
+        //     hints = document.getElementsByClassName('hint');
+        //     for (i = 0; i < hints.length; i = i + 1) {
+        //         hints[i].style.display = 'visible';
+        //     }
+        // }
     }
+    // END VALIDATION PART------------------------------------------------
 
-
-}) ();
+    geolocation();
+    formvalidation();
+}());
